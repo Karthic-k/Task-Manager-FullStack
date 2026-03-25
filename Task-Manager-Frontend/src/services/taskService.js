9cf6d7160bf241f394b32e7cb16346550f176c61
033_Alex-Pandian-M <alexpandian11205@gmail.com>
2026-03-25 08:50:42 +0530

    Added:

--- Task-Manager-Frontend/src/services/taskService.js
+++ Task-Manager-Frontend/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/tasks";
+const BASE_URL = "http://localhost:8080/tasks"; // Spring Boot backend URL
 
 export const getTasks = (status, assignedTo) => {
   return axios.get(BASE_URL, {
